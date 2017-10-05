8c460f46c8da200db3466a76ce4cb7eda8027573
Bastien <bastienrbt@protonmail.com>
2017-10-05 10:27:02 +0200

    Add password to rails_admin on local_env.yml

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Please, write the README !
 
 ## Rails template
 This app use the Bastien Robert Rails'template : [Bastien Robert](https://github.com/bastienrobert).
+
+## Get started
+1. Add graphviz to your local environment (for development)
+2. Add local_env variables for ADMIN_LOGIN and ADMIN_PASSWORD
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -21,7 +21,7 @@ RailsAdmin.config do |config|
 
   ## == Gravatar integration ==
   ## To disable Gravatar integration in Navigation Bar set to false
-  # config.show_gravatar = true
+  config.show_gravatar = false
 
   config.actions do
     dashboard                     # mandatory
@@ -37,5 +37,13 @@ RailsAdmin.config do |config|
     ## With an audit adapter, you can add:
     # history_index
     # history_show
+
+    # Authentication code (by .htaccess)
+    config.authorize_with do
+      authenticate_or_request_with_http_basic('Site Message') do |username, password|
+        username == ENV["ADMIN_LOGIN"] && password == ENV["ADMIN_PASSWORD"]
+      end
+    end
+
   end
 end
