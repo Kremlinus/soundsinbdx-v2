d163a79fbe70f46f7086bb812b62c97e482a4e26
Bastien <bastienrbt@protonmail.com>
2017-10-05 10:36:39 +0200

    Set enum for social label on rails admin

--- app/models/social.rb
+++ app/models/social.rb
@@ -2,4 +2,17 @@ class Social < ApplicationRecord
   belongs_to :about_u
   belongs_to :artist
   belongs_to :event
+
+  def label_enum
+    [
+      ['facebook'],
+      ['instagram'],
+      ['soundcloud'],
+      ['youtube'],
+      ['twitter'],
+      ['github'],
+      ['linkedin'],
+      ['tickets']
+    ]
+  end
 end
