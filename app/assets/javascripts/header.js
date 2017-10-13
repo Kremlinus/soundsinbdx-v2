5cca7cd86c06b26cf2eb58ec27553453ac122954
Kremlinus <emeline.bailleul@gmail.com>
2017-10-13 15:50:01 +0200

    Menu animation

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,28 +1,17 @@
-var modal = {
+var button = document.getElementById('menu-btn');
+var menu = document.getElementById('menu');
+var open = false;
 
-  /**
-   * Parameters
-   *
-   * @return {void}
-   */
-  _params: function () {
-
-  },
-
-
-  _init: function () {
-    this.events.openClick();
-  },
-
-  events : {
-
-    onClick : function () {
-
-    }
-
-
-  }
-
-};
-
-modal.open()
+button.addEventListener('click', function () {
+	if (open) {
+		console.log('clicked');
+		this.classList.add('cross');
+		menu.classList.add('is-open');
+		open = !open;
+	} else {
+		console.log('closed');
+		this.classList.remove('cross');
+		menu.classList.remove('is-open');
+		open = !open;
+	}
+})
