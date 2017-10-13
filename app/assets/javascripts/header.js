966fe6b2824b9b2e3f691e4dd936137ce4022048
Kremlinus <emeline.bailleul@gmail.com>
2017-10-13 11:34:14 +0200

    add burger button

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,28 @@
+var modal = {
+
+  /**
+   * Parameters
+   *
+   * @return {void}
+   */
+  _params: function () {
+
+  },
+
+
+  _init: function () {
+    this.events.openClick();
+  },
+
+  events : {
+
+    onClick : function () {
+
+    }
+
+
+  }
+
+};
+
+modal.open()
