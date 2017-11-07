61ec9b2161aace63c6cce7f975219f259f494cd4
Bastien <bastienrbt@protonmail.com>
2017-11-07 15:27:27 +0100

    Fix JS load on application layout
    It's uggly, a new method is currently in development to load the scripts

--- app/assets/javascripts/eventIndex/filters.js
+++ app/assets/javascripts/eventIndex/filters.js
@@ -1,9 +1,8 @@
 var btnFilter = document.getElementById('btnFilter'),
-	 containerFilter = document.getElementById('container-filters')
-	 containerEvent = document.getElementById('container-events')
+	 	containerFilter = document.getElementById('container-filters'),
+	 	containerEvent = document.getElementById('container-events')
 
 btnFilter.addEventListener('click', function () {
 	containerFilter.classList.toggle('is-open')
 	containerEvent.classList.toggle('down')
-
-})
+}, true)
