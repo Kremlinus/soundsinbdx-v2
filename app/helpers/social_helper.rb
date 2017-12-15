26f099ddba23c04f10b1f81599ac9774be7b76d1
emelinebailleul <emeline.bailleul@gmail.com>
2017-12-15 11:57:01 +0100

    PNG to SVG

--- app/helpers/social_helper.rb
+++ app/helpers/social_helper.rb
@@ -2,15 +2,15 @@ module SocialHelper
    def socialImageFromLabel(lbl)
       case lbl
       when 'facebook'
-         image_tag 'socials/fb-black.png'
+         image_tag 'socials/fb-black.svg'
       when 'twitter'
-         image_tag 'socials/twitter-black.png'
+         image_tag 'socials/twitter-black.svg'
       when 'github'
-         image_tag 'socials/github.png'
+         image_tag 'socials/github.svg'
       when 'instagram'
-         image_tag 'socials/instagram-black.png'
+         image_tag 'socials/instagram-black.svg'
       when 'linkedin'
-         image_tag 'socials/linkedin.png'
+         image_tag 'socials/linkedin.svg'
       else
          return ''
       end
